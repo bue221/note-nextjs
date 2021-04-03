6dfdd9651ba593294ef6b75dddf4e0781bbdffd7
bue221 <camiloplaza3@gmail.com>
2021-04-02 23:31:04 -0500

    fix bug created note

--- pages/new/index.js
+++ pages/new/index.js
@@ -8,7 +8,7 @@ const NewNote = () => {
     <FormNote
       method="POST"
       routeFetch={
-        process.env.BaseApi + "/api/note" || "http://localhost:3000/api/note"
+        process.env.BaseApi + "api/note" || "http://localhost:3000/api/note"
       }
       title="Create Note"
       btnLabel="Create"
