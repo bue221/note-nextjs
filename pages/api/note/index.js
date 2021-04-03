e32bcbd859986f0c6b3050f2fd7640c17792f93f
bue221 <camiloplaza3@gmail.com>
2021-04-02 23:43:09 -0500

    edit cors config

--- pages/api/note/index.js
+++ pages/api/note/index.js
@@ -1,23 +1,19 @@
 import { dbConnect } from "../../../backend/lib/dbConnect";
 import Note from "../../../backend/models/Note";
 
-import Cors from "cors";
-import corsInit from "../../../backend/lib/cors";
-
-// Initialize the cors middleware
-const cors = corsInit(
-  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-  Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ["GET", "POST", "OPTIONS"],
-  })
-);
+import NextCors from "nextjs-cors";
 
 dbConnect();
 
 export default async (req, res) => {
-  // Run cors
-  await cors(req, res);
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
 
   const { method } = req;
 
